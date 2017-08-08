e2e81239002d4ced3efa724895d7c31ba39db7b6
Andrew Levine <theandrewtizer@gmail.com>
2017-08-08 09:42:50 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# webpack EmitAllPlugin
+
+[webpack](https://webpack.js.org/), but without the `pack`. The `EmitAllPlugin` will emit all files in the dependency tree to `output.path` as separate files, allowing you to use loaders/plugins without the need to bundle, which can be useful for some specific use-cases.
+
+Inspired by [this tweet](https://twitter.com/thejameskyle/status/894730299845652481).
+
+## Install
+```sh
+npm install -D webpack-emit-all-plugin
+```
+
+## Usage
+In your `webpack.config.js`:
+```js
+const path = require('path');
+const EmitAllPlugin = require('webpack-emit-all-plugin');
+{
+    plugins: [
+        new EmitAllPlugin({
+            ignorePattern: /node_modules/ // default,
+            path: path.join(__dirname, 'unbundled-out') // defaults to `output.path`
+        })
+    ]
+}
+```
---
+++ index.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+module.exports = class EmitAllPlugin {
+    constructor(opts = {}) {
+        this.ignorePattern = opts.ignorePattern || /node_modules/;
+        this.path = opts.path;
+    }
+
+    shouldIgnore(path) {
+        return this.ignorePattern.test(path);
+    }
+
+    apply(compiler) {
+        compiler.plugin('after-compile', (compilation, cb) => {
+            const { modules } = compilation;
+            modules.forEach(mod => {
+                const absolutePath = mod.resource;
+                if (this.shouldIgnore(absolutePath)) return;
+
+                // Used for vendor chunk
+                if (mod.constructor.name === 'MultiModule') return;
+
+                const source = mod._source._value;
+                const projectRoot = compiler.context;
+                const out = this.path || compiler.options.output.path;
+
+                const dest = path.join(
+                    out,
+                    absolutePath.replace(projectRoot, '')
+                );
+
+                compiler.outputFileSystem.mkdirp(path.dirname(dest), err => {
+                    if (err) throw err;
+
+                    compiler.outputFileSystem.writeFile(dest, source, err => {
+                        if (err) throw err;
+                    });
+                });
+            });
+            cb();
+        });
+    }
+};
