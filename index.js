58bb93e4e72f9b646b877d6c46a1679f3f76e92d
Andrew Levine <theandrewtizer@gmail.com>
2018-05-23 17:59:00 -0500

    Prettier

--- index.js
+++ index.js
@@ -11,33 +11,43 @@ module.exports = class EmitAllPlugin {
     }
 
     apply(compiler) {
-        compiler.hooks.afterCompile.tapAsync("EmitAllPlugin", (compilation, cb) => {
-            const { modules } = compilation;
-            modules.forEach(mod => {
-                const absolutePath = mod.resource;
-                if (this.shouldIgnore(absolutePath)) return;
+        compiler.hooks.afterCompile.tapAsync(
+            'EmitAllPlugin',
+            (compilation, cb) => {
+                const { modules } = compilation;
+                modules.forEach(mod => {
+                    const absolutePath = mod.resource;
+                    if (this.shouldIgnore(absolutePath)) return;
 
-                // Used for vendor chunk
-                if (mod.constructor.name === 'MultiModule') return;
+                    // Used for vendor chunk
+                    if (mod.constructor.name === 'MultiModule') return;
 
-                const source = mod._source._value;
-                const projectRoot = compiler.context;
-                const out = this.path || compiler.options.output.path;
+                    const source = mod._source._value;
+                    const projectRoot = compiler.context;
+                    const out = this.path || compiler.options.output.path;
 
-                const dest = path.join(
-                    out,
-                    absolutePath.replace(projectRoot, '')
-                );
+                    const dest = path.join(
+                        out,
+                        absolutePath.replace(projectRoot, '')
+                    );
 
-                compiler.outputFileSystem.mkdirp(path.dirname(dest), err => {
-                    if (err) throw err;
+                    compiler.outputFileSystem.mkdirp(
+                        path.dirname(dest),
+                        err => {
+                            if (err) throw err;
 
-                    compiler.outputFileSystem.writeFile(dest, source, err => {
-                        if (err) throw err;
-                    });
+                            compiler.outputFileSystem.writeFile(
+                                dest,
+                                source,
+                                err => {
+                                    if (err) throw err;
+                                }
+                            );
+                        }
+                    );
                 });
-            });
-            cb();
-        });
+                cb();
+            }
+        );
     }
 };
